010860b93b5400de239bfc9dee4ce5afca29f792
Caitlin Ramsey <caitlinramsey@Caitlins-MacBook-Pro.local>
2023-04-15 15:25:02 -0400

    ids in HTML to just numbers to get color blocks in JS working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -28,7 +28,7 @@ Array.from(rows).forEach(row => {
     } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
       setColor(row, 'lightgrey');
     } else {
-      setColor(row, 'white');
+      setColor(row, 'lightgrey');
     }
   }
 });
@@ -44,12 +44,6 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id

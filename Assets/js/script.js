85bb37b2c8cceb878343001607bf2060abd9ba5c
Caitlin Ramsey <caitlinramsey@Caitlins-MacBook-Pro.local>
2023-04-15 14:00:57 -0400

    update

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -26,8 +26,6 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
 
 function saveScheduledEventFromStorage() {
